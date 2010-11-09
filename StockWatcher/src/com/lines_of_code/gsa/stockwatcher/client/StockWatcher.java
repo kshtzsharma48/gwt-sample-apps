4f55068932e705e14bf1d7d6b88371c5f2926e32
Christian Scholten <scholchr@gmail.com>
2010-11-09 17:30:08 +0100

    added basic panel layout

--- StockWatcher/src/com/lines_of_code/gsa/stockwatcher/client/StockWatcher.java
+++ StockWatcher/src/com/lines_of_code/gsa/stockwatcher/client/StockWatcher.java
@@ -10,23 +10,35 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class StockWatcher implements EntryPoint {
 
-  private VerticalPanel mainPanel = new VerticalPanel();
-  private FlexTable stocksFlexTable = new FlexTable();
-  private HorizontalPanel addPanel = new HorizontalPanel();
-  private TextBox newSymbolTextBox = new TextBox();
-  private Button addStockButton = new Button("Add");
-  private Label lastUpdatedLabel = new Label();
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private FlexTable stocksFlexTable = new FlexTable();
+	private HorizontalPanel addPanel = new HorizontalPanel();
+	private TextBox newSymbolTextBox = new TextBox();
+	private Button addStockButton = new Button("Add");
+	private Label lastUpdatedLabel = new Label();
 
-  /**
-   * Entry point method.
-   */
-  public void onModuleLoad() {
-    // TODO Create table for stock data.
-    // TODO Assemble Add Stock panel.
-    // TODO Assemble Main panel.
-    // TODO Associate the Main panel with the HTML host page.
-    // TODO Move cursor focus to the input box.
+	/**
+	 * Entry point method.
+	 */
+	public void onModuleLoad() {
+		// Create table for stock data.
+		stocksFlexTable.setText(0, 0, "Symbol");
+		stocksFlexTable.setText(0, 1, "Price");
+		stocksFlexTable.setText(0, 2, "Change");
+		stocksFlexTable.setText(0, 3, "Remove");
 
-  }
+		// Assemble Add Stock panel.
+		addPanel.add(newSymbolTextBox);
+		addPanel.add(addStockButton);
+
+		// Assemble Main panel.
+		mainPanel.add(stocksFlexTable);
+		mainPanel.add(addPanel);
+		mainPanel.add(lastUpdatedLabel);
+
+		// TODO Associate the Main panel with the HTML host page.
+		// TODO Move cursor focus to the input box.
+
+	}
 
 }
