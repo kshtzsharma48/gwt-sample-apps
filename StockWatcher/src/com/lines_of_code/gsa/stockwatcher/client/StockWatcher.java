8b84f8c334e36cd358cb25bb970afae930f1ca57
Christian Scholten <scholchr@gmail.com>
2010-11-10 21:14:18 +0100

    Further style changes.

--- StockWatcher/src/com/lines_of_code/gsa/stockwatcher/client/StockWatcher.java
+++ StockWatcher/src/com/lines_of_code/gsa/stockwatcher/client/StockWatcher.java
@@ -57,6 +57,7 @@ public class StockWatcher implements EntryPoint {
 		// Assemble Add Stock panel.
 		addPanel.add(newSymbolTextBox);
 		addPanel.add(addStockButton);
+		addPanel.addStyleName("addPanel");
 
 		// Assemble Main panel.
 		mainPanel.add(stocksFlexTable);
@@ -126,6 +127,12 @@ public class StockWatcher implements EntryPoint {
 		int row = stocksFlexTable.getRowCount();
 		stocks.add(symbol);
 		stocksFlexTable.setText(row, 0, symbol);
+		stocksFlexTable.getCellFormatter().addStyleName(row, 1,
+				"watchListNumericColumn");
+		stocksFlexTable.getCellFormatter().addStyleName(row, 2,
+				"watchListNumericColumn");
+		stocksFlexTable.getCellFormatter().addStyleName(row, 3,
+				"watchListRemoveColumn");
 
 		// Add a button to remove this stock from the table.
 		Button removeStockButton = new Button("x");
