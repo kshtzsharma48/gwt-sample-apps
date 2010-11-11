d4cf6c82d7d6440ebcf6b2b0a7c98745858b9fe7
Christian Scholten <scholchr@gmail.com>
2010-11-11 19:06:19 +0100

    Added StockPrice constructor test

--- StockWatcher/test/com/lines_of_code/gsa/stockwatcher/client/StockWatcherTest.java
+++ StockWatcher/test/com/lines_of_code/gsa/stockwatcher/client/StockWatcherTest.java
@@ -13,9 +13,21 @@ public class StockWatcherTest extends GWTTestCase {
 	}
 
 	/**
-	 * Ass as many tests as you like
+	 * Verify that the instance fields in the StockPrice class are set
+	 * correctly.
 	 */
-	public void testSimple() {
-		assertTrue(true);
+	public void testStockPriceCtor() {
+		String symbol = "XYZ";
+		double price = 70.0;
+		double change = 2.0;
+		double changePercent = 100.0 * change / price;
+
+		StockPrice sp = new StockPrice(symbol, price, change);
+		assertNotNull(sp);
+		assertEquals(symbol, sp.getSymbol());
+		assertEquals(price, sp.getPrice(), 0.001);
+		assertEquals(change, sp.getChange(), 0.001);
+		assertEquals(changePercent, sp.getChangePercent(), 0.001);
+
 	}
 }
