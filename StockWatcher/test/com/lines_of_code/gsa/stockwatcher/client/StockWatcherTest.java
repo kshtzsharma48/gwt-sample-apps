0668b1ad7dda0567c8db7b37cc350918f172ee61
Christian Scholten <scholchr@gmail.com>
2010-11-11 18:05:58 +0100

    Added Unittest sample

---
+++ StockWatcher/test/com/lines_of_code/gsa/stockwatcher/client/StockWatcherTest.java
@@ -0,0 +1,21 @@
+package com.lines_of_code.gsa.stockwatcher.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class StockWatcherTest extends GWTTestCase {
+
+	/**
+	 * Must refer to a valid module that sources this class.
+	 */
+	@Override
+	public String getModuleName() {
+		return "com.lines_of_code.gsa.stockwatcher.StockWatcher";
+	}
+
+	/**
+	 * Ass as many tests as you like
+	 */
+	public void testSimple() {
+		assertTrue(true);
+	}
+}
